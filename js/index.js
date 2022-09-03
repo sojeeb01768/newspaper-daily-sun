21f7cdeb3d4d7175e4ca1a12b42c1c502fdc9243
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-09-04 01:11:06 +0600

    category section

--- js/index.js
+++ js/index.js
@@ -1,27 +1,31 @@
-const loadNewsCategory =  () => {
-     fetch("https://openapi.programming-hero.com/api/news/categories")
-    .then (response => response.json())
-    .then (data => displayNewsCategory(data.data.news_category))    
+// category data load secsion
+
+const loadNewsCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then(response => response.json())
+    .then(data => displayNewsCategory(data.data.news_category))
     .catch(error => console.log(error));
-    
-}
 
-const displayNewsCategory = (catagories) =>{
-    // console.log(categories);
-    const categorydata = document.getElementById('category-list');
+}
+// category list display section
+
+const displayNewsCategory = (catagories) => {
+
+  // console.log(catagories);
+
+  const categorydata = document.getElementById('category-list');
+
+  catagories.forEach(category => {
+    // console.log(category.category_name); 
+
+    const list = document.createElement('div')
+    list.innerHTML = `
     
-    catagories.forEach(category => {
-        // console.log(category.category_name); 
-        
-        const list = document.createElement('div')
-        list.innerHTML = `
-        <button class="list-group list-group-item m-3 border-0 g-5 text-black-50 fs-5">${category.category_name}</button>
+        <button id="category-list"  onclick=" loadNews('${category.category_id}')" class="btn list-group list-group-item m-2  g-5 text-black-50 fs-5">${category.category_name}</button>
         `
-        categorydata.appendChild(list)
-    })
-    
+    categorydata.appendChild(list)
+  })
+
 }
-
-
 
 loadNewsCategory();
